ca6aeba7530e997900af2937a7b85e09a3347416
Alexandre BROSSEAU <alexandre.brosseau@edu.devinci.fr>
2023-03-22 16:20:37 +0100

    operational endpoints

--- server/api.js
+++ server/api.js
@@ -1,6 +1,10 @@
+const { Console } = require('console');
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const { connect } = require('http2');
+//const {MongoClient} = require('mongodb');
+const productsDb = require('./productsDatabase.js');
 
 const PORT = 8092;
 
@@ -17,6 +21,26 @@ app.options('*', cors());
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
+
+app.get('/products/search', async (req, res) => {
+  const limit = parseInt(req.query.limit) || 12;
+  const brand = req.query.brand || undefined;
+  const price = parseInt(req.query.price) || undefined;
+  
+  var products = await productsDb.fetchProducts(brand, price, true,false,false,limit);
+  if(products == null){console.log('no product found');products='No product found';}
+
+  res.send(products);
+});
+
+// Endpoint to get a product by UUID
+app.get('/products/*', async (req, res) => {
+  var product = await productsDb.fetchProductsByUuid(req.params[0]);
+  if(product == null){console.log('no product found');product='No product found';}
+  res.send(product);
+});
+
+
 
 app.listen(PORT);
 
---
+++ server/productsDatabase.js
@@ -0,0 +1,75 @@
+const { connect } = require('http2');
+const {MongoClient} = require('mongodb');
+const fs = require('fs');
+ 
+var MONGODB_URI = "mongodb+srv://Alexbr:Cookiemata2001@cluster0.qqzriy3.mongodb.net/test";
+const MONGODB_DB_NAME = 'clearfashion';
+var client, db, collection;
+ 
+async function connectMongoDb(){
+    console.log('Connecting to MongoDB ...');
+    client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    db =  client.db(MONGODB_DB_NAME)
+    collection = db.collection('products');
+}
+ 
+async function productsPushMongoDb(){
+    await connectMongoDb();
+    console.log('Pushing new products to MongoDB ...');
+    let rawdata = fs.readFileSync('products.json');
+    let products = JSON.parse(rawdata);
+    products.map(product => {
+        product._id = product.uuid;
+        delete product.uuid;
+    });
+    const alredyExist = await collection.find({}).toArray();
+    console.log("a");
+    products = products.filter(product => !alredyExist.some(product2 => product2._id == product._id));
+    if(products.length != 0)
+    {
+        const result = await collection.insertMany(products);
+        console.log(result);
+    }
+    else
+    {
+        console.log("No new products");
+    }
+    //process.exit(0);
+}
+ 
+async function fetchProducts(brand = undefined, maxPrice = undefined, sortedByPrice = false, sortedByDate = false, scrapedLessThanTwoWeeksAgo = false,limit = 12){
+    await connectMongoDb();
+    console.log('Fetching products from MongoDB ...');
+    var result = "none";
+    var query = {};
+    if (brand != undefined) query.brand = brand;
+    if (maxPrice != undefined) query.price = {$lt: maxPrice};
+    result = await collection.find(query);
+    if (sortedByPrice) result = result.sort({price: 1});
+    if (sortedByDate) result = result.sort({scrapDate: -1});
+    result = await result.limit(limit).toArray();
+    if (scrapedLessThanTwoWeeksAgo) result = result.filter(product => new Date(product.scrapDate) > new Date(Date.now() - 14 * 24 * 60 * 60 * 1000));
+    return result;
+    //process.exit(0);
+}
+
+
+async function fetchProductsByUuid(uuid) {
+    await connectMongoDb();
+    console.log(`Fetching product with uuid=${uuid} from MongoDB ...`);
+    const query = {_id: uuid};
+    const result = await collection.findOne(query);
+    //console.log(result);
+    return result;
+    //process.exit(0);
+  }
+  
+module.exports={
+    fetchProducts,
+    fetchProductsByUuid
+}
+ 
+//productsPushMongoDb();
+//fetchProducts("Dedicated", 10 ,true, false, false);//brand, lessThan, sortedByPrice, sortedByDate, scrapedLessThanTwoWeeksAgo
+//fetchProductsByUuid('b3b12d7be201a8e7d029c557a2b4bc73');
+//fetchProducts2("Dedicated", 100 ,true,false,false);
